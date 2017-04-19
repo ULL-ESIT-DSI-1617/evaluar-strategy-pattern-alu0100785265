fcb65a002bdbcd86e31a9afdf93dce90c31e4a00
alu0100785265 <alu0100785265@ull.edu.es>
2017-04-19 15:17:02 +0000

    cambios y travis

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var getArea = require('./shapesarea.js');
+var figura = require('./shape.js');
+var triangle = require('./triangle.js');
+var rectangle = require('./square.js');
+var rectangle = require('./rectangle.js');
+
+  "use strict"
 
 try {
-  var t = getArea('Triangle',  { width: 100, height: 100 });
+  var t = figura.r_figura('Triangle',  { width: 100, height: 100 });
   console.log(t);
-  var s = getArea('Square',    { width: 100 });
+  var s = figura.r_figura('Square',    { width: 100 });
   console.log(s);
-  var r = getArea('Rectangle', { width: 100, height: 100 });
+  var r = figura.r_figura('Rectangle', { width: 100, height: 100 });
   console.log(r);
-  var b = getArea('Bogus');
+  //var b = figura.r_figura('Bogus');
 }
 catch (e) {
   console.log(e);
 }
-
-
